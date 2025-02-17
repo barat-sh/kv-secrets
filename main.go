82d8677c2daa0e39d0bf8593cfcd43fa3a453f62
Barath <barath@ariveguru.com>
2025-02-17 18:20:12 +0530

    debuging

--- main.go
+++ main.go
@@ -1,22 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 	"os"
-  "fmt"
-  "net/http"
+
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-  "github.com/gin-gonic/gin"
 
 	"github.com/go-resty/resty/v2"
 )
 
 var client = resty.New()
 
-func writeToKv(c *gin.Context){
-  key := c.Param("key")
-  value := c.PostForm("value")
-  connectionURL, exists := os.LookupEnv("KV_CONNECTION_URL")
+func writeToKv(c *gin.Context) {
+	key := c.Param("key")
+	value := c.PostForm("value")
+	connectionURL, exists := os.LookupEnv("KV_CONNECTION_URL")
 	if !exists {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Missing KV_CONNECTION_URL"})
 		return
@@ -38,24 +39,24 @@ func writeToKv(c *gin.Context){
 	if !exists {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Missing KV_API_TOKEN"})
 		return
-	} 
-  fmt.Println(key)
-  fmt.Println(value)
-  url := fmt.Sprintf(connectionURL, accountID, namespaceID, key)
+	}
+	fmt.Println(key)
+	fmt.Println(value)
+	url := fmt.Sprintf(connectionURL, accountID, namespaceID, key)
 
-  resp, err := client.R().SetAuthToken(apiToken).SetHeader("Content-Type", "text/plain").SetBody(value).Put(url)
+	resp, err := client.R().SetAuthToken(apiToken).SetHeader("Content-Type", "text/plain").SetBody(value).Put(url)
 
-  if err != nil{
-    c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-    return
-  }
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
 
-  c.Data(resp.StatusCode(), "application/json", []byte(resp.String()))
+	c.Data(resp.StatusCode(), "application/json", []byte(resp.String()))
 }
 
 func readFromKv(c *gin.Context) {
-  key := c.Param("key")
-  connectionURL, exists := os.LookupEnv("KV_CONNECTION_URL")
+	key := c.Param("key")
+	connectionURL, exists := os.LookupEnv("KV_CONNECTION_URL")
 	if !exists {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Missing KV_CONNECTION_URL"})
 		return
@@ -78,17 +79,17 @@ func readFromKv(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Missing KV_API_TOKEN"})
 		return
 	}
-  url := fmt.Sprintf(connectionURL, accountID, namespaceID, key)
+	url := fmt.Sprintf(connectionURL, accountID, namespaceID, key)
 
-  resp, err := client.R().SetAuthToken(apiToken).Get(url)
+	resp, err := client.R().SetAuthToken(apiToken).Get(url)
 
-  if err != nil {
-    c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-    return
-  }
-  fmt.Println("result", resp.StatusCode())
-  fmt.Println("result", resp.String())
-  c.Data(resp.StatusCode(), "application/json", []byte(resp.String()))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	fmt.Println("result", resp.StatusCode())
+	fmt.Println("result", resp.String())
+	c.Data(resp.StatusCode(), "application/json", []byte(resp.String()))
 }
 
 // func deleteFromKv(c *gin.Context){
@@ -106,26 +107,21 @@ func readFromKv(c *gin.Context) {
 //   c.Data(resp.StatusCode(), "application/json", []byte(resp.String()))
 // }
 
-
 func main() {
 
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found, using default environment variables")
 	}
 
-  app := gin.Default()
+	app := gin.Default()
 
-  app.GET("/:key", readFromKv)
+	app.GET("/:key", readFromKv)
 
-  app.POST("/:key", writeToKv)
+	app.POST("/:key", writeToKv)
 
-  // app.DELETE("/:key", deleteFromKv)
+	// app.DELETE("/:key", deleteFromKv)
 
-  PORT := os.Getenv("PORT")
-  if PORT == ""{
-    PORT = 
-    "8080"
-  }
-  fmt.Println("Server running on PORT -> ", PORT)
-  log.Fatal(app.Run(":" + PORT))
+	PORT := os.Getenv("PORT")
+	fmt.Println("Server running on PORT -> ", PORT)
+	log.Fatal(app.Run(":" + PORT))
 }
